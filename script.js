cfa0633b77d68201c7d1b80e1acae99fde9ebf1c
djbohl <96339436+djbohl@users.noreply.github.com>
2022-10-02 05:58:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,34 +6,6 @@ function buttonFunction() {
     if (registerBttn.onclick) {
         location.assign("register.html");
     } else if (loginBttn.onclick) {
-        location.assign("main.html");
+        location.assign("index.html");
     }
 }
-
-/*
-
-//disable log in automatically
-document.getElementById("log-in").disabled = true;
-
-when register button is clicked 
-remove disabled and disable register 
-
-
-document.getElementById('register').addEventListener('change', function() {
-    if (document.getElementById('register').clicked) {
-      document.getElementById('log-in').removeAttribute("disabled"),
-        document.getElementById('register').addAttribute("disabled");
-    }
-  });
-  
-  when register button is clicked 
-remove disabled and disable register 
-
-document.getElementById('log-in').addEventListener('change', function() {
-    if (document.getElementById('log-in').clicked) {
-      document.getElementById('register').removeAttribute("disabled"),
-        document.getElementById('log-in').addAttribute("disabled");
-    }
-  });
-  
-  */
