b43a39d96c4f116ddd37f09a863e65a371b1b40d
djbohl <96339436+djbohl@users.noreply.github.com>
2022-10-08 21:33:34 -0400

    deleted file, created component to replace

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-//login and register buttons
-
-function buttonFunction() {
-    let loginBttn = document.getElementById('log-in');
-    let registerBttn = document.getElementById('register');
-    if (registerBttn.onclick) {
-        location.assign("register.html");
-    } else if (loginBttn.onclick) {
-        location.assign("index.html");
-    }
-}
+//create login and register button function to hide and show component
